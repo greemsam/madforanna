9edbdd208cca5cb283260d6a08be05f47797a2b5
Sungyoon Jung <30921903+greemsam@users.noreply.github.com>
2020-07-17 23:03:26 +0900

    Update madforanna.js

--- js/madforanna.js
+++ js/madforanna.js
@@ -1,44 +1,39 @@
 var madness;
+var executeButton = document.getElementById('execute');
+var echoDiv = document.getElementById('echo');
+var trigger = false;
 var madnessOn = new Vue({
   el:'#madness',
-  data:{
-    trigger:0
-  },
+  data:{},
   methods:{
-    setTrigger:()=>{
-      madnessOn.trigger++;
-      if(madnessOn.trigger>1){
-        madnessOn.trigger = 0;
+    setMadness:()=>{
+      trigger = !trigger;
+      switch(trigger){
+        case true: 
+        madness = setInterval(madnessOn.createAnnaTag, 30);
+        executeButton.innerHTML = '광기중지';
+        break;
+        case false:
+        clearInterval(madness);
+        executeButton.innerHTML = '계속 찬양하기'
+        break;
       }
+      // console.log(trigger);
     },
-    textWrite:(triggerValue)=>{
-      var executeButton = document.getElementById('execute');
-      var theMaxNumberOfAnna = 0;
-      while(theMaxNumberOfAnna < 300){
-        if(triggerValue === 1){
-          theMaxNumberOfAnna++;
-          madness = setInterval(madnessOn.createAnna, 30);
-          executeButton.innerHTML = '광기중지'
-        }else{
-          clearInterval(madness);
-          executeButton.innerHTML = '계속 찬양하기'
-        }
-        console.log(theMaxNumberOfAnna);
-      }
-    },
-    createAnna:()=>{
+    createAnnaTag:()=>{
       var echoDiv = document.getElementById('echo');
       var annaSpan = document.createElement('span');
-      var getRandom = Math.floor(Math.random() * 2);
       var annaText = document.createTextNode('안나!!');
       var queenText = document.createTextNode('여왕님!!');
+      echoDiv.appendChild(annaSpan);
+      var getRandom = Math.floor(Math.random() * 2);
       switch(getRandom){
         case 0 : annaSpan.appendChild(annaText);
         break;
         case 1 : annaSpan.appendChild(queenText);
         break;
       }
-      echoDiv.appendChild(annaSpan);
-    },
-  },
+      // console.log(annaSpan.length);
+    }
+  }
 });
