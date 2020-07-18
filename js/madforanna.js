f3637ba21bfa195f24f3ea8afd964e7f7d63ec48
Sungyoon Jung <30921903+greemsam@users.noreply.github.com>
2020-07-18 13:22:51 +0900

    Add files via upload

--- js/madforanna.js
+++ js/madforanna.js
@@ -1,29 +1,31 @@
 var madness;
-var executeButton = document.getElementById('execute');
+var echoDiv;
+var numOfEchoes = 0;
+var echoLimit = 621;
 var trigger = false;
 var madnessOn = new Vue({
   el:'#madness',
   data:{},
   methods:{
     setMadness:()=>{
-      trigger = !trigger;
-      switch(trigger){
-        case true: 
-        madness = setInterval(madnessOn.createAnnaTag, 30);
-        executeButton.innerHTML = '광기중지';
-        break;
-        case false:
-        clearInterval(madness);
-        executeButton.innerHTML = '계속 찬양하기'
-        break;
+      if(numOfEchoes < echoLimit){
+        trigger = !trigger;
+        madnessOn.changeButtonText(trigger);
+        switch(trigger){
+          case true: madness = setInterval(madnessOn.createAnnaTag, 10);
+          break;
+          case false: clearInterval(madness);
+          break;
+        }
       }
-      // console.log(trigger);
     },
     createAnnaTag:()=>{
       var echoDiv = document.getElementById('echo');
       var annaSpan = document.createElement('span');
       var annaText = document.createTextNode('안나!!');
       var queenText = document.createTextNode('여왕님!!');
+      numOfEchoes++;
+      madnessOn.changeButtonText(trigger);
       echoDiv.appendChild(annaSpan);
       var getRandom = Math.floor(Math.random() * 2);
       switch(getRandom){
@@ -32,7 +34,33 @@ var madnessOn = new Vue({
         case 1 : annaSpan.appendChild(queenText);
         break;
       }
-      // console.log(annaSpan.length);
-    }
+      if(numOfEchoes >= echoLimit){
+        trigger = false;
+        clearInterval(madness);
+      }
+    },
+    changeButtonText:(triggerValue)=>{
+      var mednessButton = document.getElementById('execute');
+      if(triggerValue === true && numOfEchoes < echoLimit){
+        mednessButton.innerHTML = '광기 중지하기';
+      }
+      else if(triggerValue === false && numOfEchoes < echoLimit){
+        mednessButton.innerHTML = '계속 찬양하기'
+      }
+      else if(numOfEchoes >= echoLimit){
+        mednessButton.innerHTML = '더 이상 찬양불가'
+      }
+    },
   }
 });
+
+// window.onscroll = function(){
+//   var buttonWrap = document.getElementById('button-wrap');
+//   var contentArea = document.getElementById('content-area').offsetHeight;
+//   if(window.scrollY > contentArea){
+//     buttonWrap.setAttribute('class', 'fixed');
+//   }
+//   else{
+//     buttonWrap.classList.remove('fixed');
+//   }
+// }
